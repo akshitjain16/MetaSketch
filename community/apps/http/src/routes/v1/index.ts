8c23ea15b3582f93f48e6ed7ccc389ee5b4198c9
akshitjain16 <gang.akshitjain@gmail.com>
2024-11-10 00:22:22 +0530

    authentication backend completed with tests

--- community/apps/http/src/routes/v1/index.ts
+++ community/apps/http/src/routes/v1/index.ts
@@ -2,19 +2,79 @@ import { Router } from "express";
 import { userRouter } from "./user";
 import { adminRouter } from "./admin.";
 import { spaceRouter } from "./space";
-
+import { SigninSchema, SignupSchema } from "../../types";
+import  client from "@repo/db/client";
+import jwt from "jsonwebtoken";
+import { JWT_PASSWORD } from "../../config";
+import { compare, hash } from "../../scrypt";
 export const router =  Router();
 
-router.post("/signup", (req,res)=>{
-  res.json({
-    message: "SignUp"
-  })
+router.post("/signup", async (req, res) => {
+  console.log("inside signup")
+  // check the user
+  const parsedData = SignupSchema.safeParse(req.body)
+  if (!parsedData.success) {
+      console.log("parsed data incorrect")
+      res.status(400).json({message: "Validation failed"})
+      return
+  }
+
+  const hashedPassword = await hash(parsedData.data.password)
+
+  try {
+       const user = await client.user.create({
+          data: {
+              username: parsedData.data.username,
+              password: hashedPassword,
+              role: parsedData.data.type === "admin" ? "Admin" : "User",
+          }
+      })
+      res.json({
+          userId: user.id
+      })
+  } catch(e) {
+      console.log("error thrown")
+      console.log(e)
+      res.status(400).json({message: "User already exists"})
+  }
 })
 
-router.post("/signin", (req,res)=>{
-  res.json({
-    message: "SignIn"
-  })
+router.post("/signin", async (req, res) => {
+  const parsedData = SigninSchema.safeParse(req.body)
+  if (!parsedData.success) {
+      res.status(403).json({message: "Validation failed"})
+      return
+  }
+
+  try {
+      const user = await client.user.findUnique({
+          where: {
+              username: parsedData.data.username
+          }
+      })
+      
+      if (!user) {
+          res.status(403).json({message: "User not found"})
+          return
+      }
+      const isValid = await compare(parsedData.data.password, user.password)
+
+      if (!isValid) {
+          res.status(403).json({message: "Invalid password"})
+          return
+      }
+
+      const token = jwt.sign({
+          userId: user.id,
+          role: user.role
+      }, JWT_PASSWORD);
+
+      res.json({
+          token
+      })
+  } catch(e) {
+      res.status(400).json({message: "Internal server error"})
+  }
 })
 
 router.get("/elements", (req,res)=>{
